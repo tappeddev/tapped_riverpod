bd6c0a569b4a6ccc599a3c9d3aa899b386caa192
Stefan Schaller <stefan.schaller@tapped.dev>
2025-11-25 08:20:24 +0100

    remove extendsion and expose riverpod stuff

--- lib/tapped_riverpod.dart
+++ lib/tapped_riverpod.dart
@@ -6,3 +6,7 @@ export 'src/base_notifier.dart';
 export 'src/result.dart';
 
 // endregion
+
+export 'package:riverpod/riverpod.dart';
+export 'package:riverpod/legacy.dart';
+export 'package:riverpod/misc.dart';
