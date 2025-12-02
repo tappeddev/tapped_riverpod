0bc9743857301b1a0aafb0b70a2a8b9be95d4feb
Stefan Schaller <stefan.schaller@tapped.dev>
2025-12-02 15:49:02 +0100

    fix tests

--- test/result_filter_notifier.dart
+++ test/result_filter_notifier.dart
@@ -4,161 +4,87 @@ import 'package:test/test.dart';
 
 void main() {
   group('ResultFilterNotifier', () {
-    test('the initial value should be correct ', () {
-      final inner = StateProvider<Result<String>>(
-        (_) => Result.success("Initial-Success"),
-      );
-
-      final provider = NotifierProvider(
-        () => ResultFilterNotifier<String>(
-          result: inner,
-          filterMap: (r) => r.whenOrNull(success: (v) => v),
-        ),
-      );
-
-      final container = ProviderContainer();
-      addTearDown(container.dispose);
-
-      final value = container.read(provider);
-
-      expect(value, "Initial-Success");
-    });
-
-    test('does not change state when filterMap returns null', () {
-      final inner = StateProvider<Result<String>>(
-        (_) => Result.success('data'),
-      );
-
-      final provider = NotifierProvider(
-        () => ResultFilterNotifier<String>(
-          result: inner,
-          // filterMap returns null always => no updates should be applied after build
-          filterMap: (r) => r.whenOrNull(success: (v) => v),
-        ),
-      );
-
-      final container = ProviderContainer();
-      addTearDown(container.dispose);
-
-      // initial build => filterMap returns null, initial state should be null
-      expect(container.read(provider), "data");
-
-      container.read(inner.notifier).state = Result.loading();
-      container.read(inner.notifier).state = Result.failure(
-        DisplayableError(
-          exception: Exception("Expected error"),
-          stackTrace: StackTrace.current,
-        ),
-      );
-
-      //TODO can check how many times this was called ?
-      expect(container.read(provider), "data");
-    });
-
-    test('does not change state when filterMap returns the same data again', () {
-      final inner = StateProvider<Result<String>>(
-        (_) => Result.success('data'),
-      );
-
-      final provider = NotifierProvider(
-        () => ResultFilterNotifier<String>(
-          result: inner,
-          // filterMap returns null always => no updates should be applied after build
-          filterMap: (r) => r.whenOrNull(success: (v) => v),
-        ),
-      );
-
-      final container = ProviderContainer();
-
-      addTearDown(container.dispose);
-
-      // initial build => filterMap returns null, initial state should be null
-      expect(container.read(provider), "data");
-
-      container.read(inner.notifier).state = Result.success("data");
-      container.read(inner.notifier).state = Result.success("data");
-
-      //TODO can check how many times this was called ?
-      expect(container.read(provider), "data");
-    });
-
-    test(
-      'updates state when inner emits success mapping to non-null',
-      () async {
-        final inner = StateProvider<Result<String>>(
-          (_) => Result.success("initial-data"),
-        );
-
-        final provider = NotifierProvider(
-          () => ResultFilterNotifier<String>(
-            result: inner,
-            filterMap: (r) => r.whenOrNull(success: (v) => v),
-          ),
-        );
-
-        final container = ProviderContainer();
-        addTearDown(container.dispose);
-
-        expect(container.read(provider), "initial-data");
-
-        container.read(inner.notifier).state = Result.success("data-2");
-
-        //TODO can check how many times this was called ?
-        expect(container.read(provider), "data-2");
-
-        container.read(inner.notifier).state = Result.success("data-3");
-
-        //TODO can check how many times this was called ?
-        expect(container.read(provider), "data-3");
-      },
+    _test(
+      testName: "initial value should be correct",
+      initialState: ResultSuccess("Initial-Data"),
+      fireUpdated: (_) {},
+      expectedOutputs: ["Initial-Data"],
     );
 
-    test(
-      'updates state when mapped value changed',
-          () async {
-        final inner = StateProvider<Result<String>>(
-              (_) => Result.success("initial-data"),
-        );
-
-        final provider = NotifierProvider(
-              () => ResultFilterNotifier<String>(
-            result: inner,
-            filterMap: (r) => r.whenOrNull(success: (v) => v),
-          ),
-        );
-
-        final container = ProviderContainer();
-        addTearDown(container.dispose);
-
-        expect(container.read(provider), "initial-data");
-
-        container.read(inner.notifier).state = Result.loading();
-        container.read(inner.notifier).state = Result.failure(
-          DisplayableError(
-            exception: Exception("Expected error"),
-            stackTrace: StackTrace.current,
-          ),
-        );
-        container.read(inner.notifier).state = Result.loading();
-        container.read(inner.notifier).state = Result.success("data-2");
-
-        //TODO can check how many times this was called ?
-        expect(container.read(provider), "data-2");
-
-
-        container.read(inner.notifier).state = Result.loading();
-        container.read(inner.notifier).state = Result.failure(
-          DisplayableError(
-            exception: Exception("Expected error"),
-            stackTrace: StackTrace.current,
-          ),
-        );
-        container.read(inner.notifier).state = Result.loading();
-        container.read(inner.notifier).state = Result.success("data-3");
-
-        //TODO can check how many times this was called ?
-        expect(container.read(provider), "data-3");
+    _test(
+      testName: "does not change state when filterMap returns null",
+      initialState: ResultSuccess("Initial-Data"),
+      fireUpdated: (prov) {
+        prov
+          ..setResult(Result.loading())
+          ..setResult(
+            Result.failure(
+              DisplayableError(
+                exception: Exception("Expected error"),
+                stackTrace: StackTrace.current,
+              ),
+            ),
+          );
       },
+      expectedOutputs: ["Initial-Data"],
+    );
+
+    _test(
+      testName: "should not change state when same value is reported again",
+      initialState: ResultSuccess("Data"),
+      fireUpdated: (prov) {
+        prov
+          ..setResult(Result.success("Data"))
+          ..setResult(Result.success("Data"));
+      },
+      expectedOutputs: ["Data"],
     );
   });
+}
+
+void _test({
+  required String testName,
+  required Result<String> initialState,
+  required void Function(_TestBaseNotifier prov) fireUpdated,
+  required List<String?> expectedOutputs,
+}) {
+  test(testName, () {
+    final inner = NotifierProvider(() => _TestBaseNotifier(initialState));
+
+    final provider = NotifierProvider(
+      () => ResultFilterNotifier<String>(
+        result: inner,
+        filterMap: (r) => r.whenOrNull(success: (v) => v),
+      ),
+    );
+
+    final container = ProviderContainer();
+
+    addTearDown(container.dispose);
+
+    final events = <String?>[];
+
+    container.listen<String?>(
+      provider,
+      (previous, next) => events.add(next),
+      fireImmediately: true,
+    );
+
+    fireUpdated(container.read(inner.notifier));
+
+    expect(events, expectedOutputs);
+  });
+}
+
+class _TestBaseNotifier extends BaseNotifier<Result<String>> {
+  final Result<String> _initial;
+
+  _TestBaseNotifier(this._initial);
+
+  @override
+  Result<String> init() => _initial;
+
+  void setResult(Result<String> result) {
+    state = result;
+  }
 }
